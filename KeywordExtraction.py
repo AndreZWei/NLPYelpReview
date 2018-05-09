167841dd1707f6542c2eaab536292aecc817dcb8
AndreZWei <zhangyang.wei2013@gmail.com>
2018-05-09 14:54:32 -0400

    clean up; edited readme

--- KeywordExtraction.py
+++ KeywordExtraction.py
@@ -10,6 +10,10 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import wordnet
 from nltk.wsd import lesk
+import gensim
+
+
+
 
 
 ############
@@ -29,6 +33,7 @@ with open('data/id_content.data', 'rb') as input:
 
 all_tokens = []
 all_reviews = []
+all_sentences = []
 
 iterator = iter(business_review)
 for i in range(len(business_review)):
@@ -36,12 +41,15 @@ for i in range(len(business_review)):
 	tokens = []
 	for review_id in business_review[next(iterator)]:
 		# Remove punctuations and uppercase all sentences
+		all_sentences = all_sentences + nltk.sent_tokenize(id_content[review_id]["text"])
 		text = id_content[review_id]["text"].translate(str.maketrans("", "", string.punctuation)).upper()
 		review = review + text
 		tokens = tokens + nltk.word_tokenize(text)
 	all_reviews.append(review)
 	all_tokens.append(tokens)
 
+with open('data/all_sentences.data', 'wb') as output:
+	pickle.dump(all_sentences, output)
 
 
 
@@ -52,6 +60,8 @@ for i in range(len(business_review)):
 # Task: For each business get common ngrams (up to 3)
 # Method: nltk collocations (pmi)
 
+
+# To test part 1 alone, uncomment line 65 - 75 to read in the saved data
 '''
 
 # A list of strings, each corresponding to all reviews of a business
@@ -94,9 +104,6 @@ for i in range(len(all_tokens)):
 	# Note: We do not simply remove bigrams and trigrams with stopwords because dish names may include stopwords, e.g. fish and chips.
 
 
-
-
-
 ############
 ## Part 2 ##
 ############
@@ -104,6 +111,7 @@ for i in range(len(all_tokens)):
 # Method: TF-IDF
 
 
+# To test part 2 alone, uncoment line 115 - 139 to read in the data
 '''
 
 # A list of strings, each corresponding to all reviews of a business
@@ -126,13 +134,13 @@ with open('data/bigrams.data', 'rb') as input:
 with open('data/trigrams.data', 'rb') as input:
 	trigrams = pickle.load(input)
 
-print("loading file done")
+# print("loading file done")
 
-'''
+
 
 # Find the terms with a non-zero entry in the term-document matrix
 # These are terms that bear important document-specific information
-
+'''
 
 tfidf_unigram = TfidfVectorizer(ngram_range = (1,1))
 doc_term_matrix = tfidf_unigram.fit_transform(all_reviews)
@@ -162,13 +170,15 @@ for i in range(10):
 
 
 
-
 ############
 ## Part 3 ##
 ############
 # Task 3: Determine if ngrams are actually dishes
 
+
+# To test part 3 alone, uncomment line 180 - 197 to read in the data
 '''
+
 # Read in data
 # list of filtered unigrams
 with open('data/unigrams_filtered.data', 'rb') as input:
@@ -186,6 +196,14 @@ print("loading file done")
 
 '''
 
+# Some example words we manually selected
+example_unigram_dish_names = ['steak', 'fish', 'cake', 'coffee', 'duck', 'carrot', 'pasta', 'shrimp', 'apple', 'bagel']
+example_bigram_dish_names = [('pulled', 'pork'), ('Brussels', 'sprouts'), ('grain', 'mustard'), ('Kobe', 'beef'), ('orange', 'juice')]
+example_trigram_dish_names = [('caramel', 'creme', 'brulee'), ('squeezed', 'orange', 'juice'), ('braised', 'short', 'rib'), ('vanilla', 'ice', 'cream')]
+
+
+
+'''
 # Method 1: WordNet
 # We find the dish terms based on its semantic similarity with another dish name
 # For each type of n-grams, we manually select one term that is a dish as our example, then we use wordNet to find all words with high similarity with our example
@@ -204,40 +222,55 @@ def unigram_similarity(word1, word2):
 		return 0
 	return sim
 
-unigram_dishes = []
-example_dish_name = 'steak'
-for unigram_list in unigrams:
-	unigram_dish = []
-	for i in range(len(unigram_list)):
-		if (unigram_similarity(unigram_list[i], example_dish_name) > 0.5):
-			unigram_dish.append(unigram_list[i])
-	unigram_dishes.append(unigram_dish)
-print("unigram dishes finished")
-
 # We define the similarity between bigrams the max similarity bewteen each individual words
 def bigram_similarity(bigram1, bigram2):
 	return max(unigram_similarity(bigram1[0], bigram2[0]), unigram_similarity(bigram1[0], bigram2[1]), unigram_similarity(bigram1[1], bigram2[0]), unigram_similarity(bigram1[1], bigram2[1]))
-
-bigram_dishes = []
-example_dish_name = ('pulled', 'pork')
-for bigram_list in bigrams:
-	bigram_dish = []
-	for i in range(len(bigram_list)):
-		if (bigram_similarity(bigram_list[i], example_dish_name) > 0.5):
-			bigram_dish.append(bigram_list[i][0] + ' ' + bigram_list[i][1])
-	bigram_dishes.append(bigram_dish)
-print("bigram dishes finished")
 
 # We give a similar definition for trigram similarities
 def trigram_similarity(trigram1, trigram2):
 	return max(unigram_similarity(trigram1[0], trigram2[0]), unigram_similarity(trigram1[0], trigram2[1]), unigram_similarity(trigram1[0], trigram2[2]), unigram_similarity(trigram1[1], trigram2[0]), unigram_similarity(trigram1[1], trigram2[1]), unigram_similarity(trigram1[1], trigram2[2]), unigram_similarity(trigram1[2], trigram2[0]), unigram_similarity(trigram1[2], trigram2[1]), unigram_similarity(trigram1[2], trigram2[2]))
 
+# We say a word is similar to the words in example word list
+def similarity(word, word_list, ngram):
+	max_score = 0
+	if ngram == 1:
+		for example_word in word_list:
+			max_score = max(unigram_similarity(word, example_word), max_score)
+	if ngram == 2:
+		for example_word in word_list:
+			max_score = max(bigram_similarity(word, example_word), max_score)
+	if ngram == 3:
+		for example_word in word_list:
+			max_score = max(trigram_similarity(word, example_word), max_score)
+	return max_score
+
+
+
+unigram_dishes = []
+for unigram_list in unigrams:
+	unigram_dish = []
+	for i in range(len(unigram_list)):
+		if (similarity(unigram_list[i], example_unigram_dish_names, 1) > 0.7):
+			unigram_dish.append(unigram_list[i])
+	unigram_dishes.append(unigram_dish)
+print("unigram dishes finished")
+
+# find the bigram dishes
+bigram_dishes = []
+for bigram_list in bigrams:
+	bigram_dish = []
+	for i in range(len(bigram_list)):
+		if (similarity(bigram_list[i], example_bigram_dish_names, 2) > 0.7):
+			bigram_dish.append(bigram_list[i][0] + ' ' + bigram_list[i][1])
+	bigram_dishes.append(bigram_dish)
+print("bigram dishes finished")
+
+# find trigram dishes
 trigram_dishes = []
-example_dish_name = ('caramel', 'creme', 'brulee')
 for trigram_list in trigrams:
 	trigram_dish = []
 	for i in range(len(trigram_list)):
-		if (trigram_similarity(trigram_list[i], example_dish_name) > 0.5):
+		if (similarity(trigram_list[i], example_trigram_dish_names, 3) > 0.7):
 			trigram_dish.append(trigram_list[i][0] + ' ' + trigram_list[i][1] + ' ' + trigram_list[i][2])
 	trigram_dishes.append(trigram_dish)
 print("trigram dishes finished")
@@ -247,7 +280,100 @@ wordnet_dishes = []
 for i in range(10):
 	wordnet_dishes.append(unigram_dishes[i] + bigram_dishes[i] + trigram_dishes[i])
 
+print(wordnet_dishes)
 
-# Method 2: Gensim.word2vec
+'''
 
+# Uncomment below (line 293 - 304) to test Method 2
+
+
+# Method 2: Gensim.Word2Vec
+
+
+# Train the model using all reviews
+
+with open('data/all_sentences.data', 'rb') as input:
+	all_sentences = pickle.load(input)
+
+print(len(all_sentences))
+
+all_tokenized_sentences = list(map(lambda x: nltk.word_tokenize(x), all_sentences))
+
+# Building the model
+
+model = gensim.models.Word2Vec(all_tokenized_sentences, size=150, window=10, min_count=2, workers=10)
+print("model building done")
+
+model.save("review.model")
+
+
+# Use the model to get word similarity
+model = gensim.models.Word2Vec.load("review.model")
+
+
+def unigram_similarity(word1, word2):
+	try:
+		sim = model.wv.similarity(word1, word2)
+	except KeyError:
+		return 0
+	return sim
+
+# We define the similarity between bigrams the max similarity bewteen each individual words
+def bigram_similarity(bigram1, bigram2):
+	return max(unigram_similarity(bigram1[0], bigram2[0]), unigram_similarity(bigram1[0], bigram2[1]), unigram_similarity(bigram1[1], bigram2[0]), unigram_similarity(bigram1[1], bigram2[1]))
+
+# We give a similar definition for trigram similarities
+def trigram_similarity(trigram1, trigram2):
+	return max(unigram_similarity(trigram1[0], trigram2[0]), unigram_similarity(trigram1[0], trigram2[1]), unigram_similarity(trigram1[0], trigram2[2]), unigram_similarity(trigram1[1], trigram2[0]), unigram_similarity(trigram1[1], trigram2[1]), unigram_similarity(trigram1[1], trigram2[2]), unigram_similarity(trigram1[2], trigram2[0]), unigram_similarity(trigram1[2], trigram2[1]), unigram_similarity(trigram1[2], trigram2[2]))
+
+# We say a word is similar to the words in example word list
+def similarity(word, word_list, ngram):
+	max_score = 0
+	if ngram == 1:
+		for example_word in word_list:
+			max_score = max(unigram_similarity(word, example_word), max_score)
+	if ngram == 2:
+		for example_word in word_list:
+			max_score = max(bigram_similarity(word, example_word), max_score)
+	if ngram == 3:
+		for example_word in word_list:
+			max_score = max(trigram_similarity(word, example_word), max_score)
+	return max_score
+
+
+unigram_dishes = []
+for unigram_list in unigrams:
+	unigram_dish = []
+	for i in range(len(unigram_list)):
+		if (similarity(unigram_list[i], example_unigram_dish_names, 1) > 0.5):
+			unigram_dish.append(unigram_list[i])
+	unigram_dishes.append(unigram_dish)
+print("unigram dishes finished")
+
+# find the bigram dishes
+bigram_dishes = []
+for bigram_list in bigrams:
+	bigram_dish = []
+	for i in range(len(bigram_list)):
+		if (similarity(bigram_list[i], example_bigram_dish_names, 2) > 0.5):
+			bigram_dish.append(bigram_list[i][0] + ' ' + bigram_list[i][1])
+	bigram_dishes.append(bigram_dish)
+print("bigram dishes finished")
+
+# find trigram dishes
+trigram_dishes = []
+for trigram_list in trigrams:
+	trigram_dish = []
+	for i in range(len(trigram_list)):
+		if (similarity(trigram_list[i], example_trigram_dish_names, 3) > 0.5):
+			trigram_dish.append(trigram_list[i][0] + ' ' + trigram_list[i][1] + ' ' + trigram_list[i][2])
+	trigram_dishes.append(trigram_dish)
+print("trigram dishes finished")
+
+# Generate a single list for each restaurant
+word2vec_dishes = []
+for i in range(10):
+	word2vec_dishes.append(unigram_dishes[i] + bigram_dishes[i] + trigram_dishes[i])
+
+print(word2vec_dishes)
 
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # NLPYelpReview
+
+### Author: Boyang Lu, Zhangyang Wei, Jie Zhou
+
+The project performs two tasks:
+
+1. Extract food-related keywords from yelp reviews
+
+2. Correlate yelp ratings with review sentiments
+
+
+Data:
+We used the data from http://www.yelp.com/dataset. We processed and serialized the data. The stored data files are in the data folder.
+
+
+## Task 1: KeywordExtraction.py
+
+### Part 0: Tokenize the reviews
+### Part 1: Use nltk to find the unigram, bigram and trigram collocations
+### Part 2: Use tf-idf to find the collocations that are specific to each document (business)
+### Part 3: Find food-related collocations
+#### Method 1: wordnet
+#### Method 2: word2vec
+
+#### Note: It takes a long time to run the entire program (~30 mins on my machine), but each part is designed to be tested separately, given the data needed. In the code before each part there is a section for reading in the data. To test the a single part of the program, uncomment the section for reading in the data and comment out all the other parts. The two methods in part 3 are not designed to be tested together. Please ensure that one method is commented out completely when testing the other one.
+
+
+## Task 2: SentimentScore.py
+
+#### We used the textblob sentiment function. The code can be directly executed and tested.
